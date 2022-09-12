daf6effc00b2fa2c900931c4875c7eef1077f76d
Ayush Kumar <github@ayushk.xyz>
2022-09-12 07:49:35 -0700

    Add comments

--- src/command.cr
+++ src/command.cr
@@ -3,17 +3,20 @@ module Quark
   module Command
     # run command
     def self.run(command, args)
-      # commands and methods
       case command
+      # run quark
       when "run"
         run_quark(args)
 
+      # run shell hook
       when "hook"
         run_hook(args)
 
+      # add location
       when "add"
         add_location
 
+      # list locations
       when "list"
         list_locations
 
