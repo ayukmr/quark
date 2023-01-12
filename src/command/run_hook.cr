cb68c4ed9084ade868677153c6e95fed42c8e1be
Ayush Kumar <github@ayushk.xyz>
2023-01-11 16:53:15 -0800

    Store hooks as hash

--- src/command/run_hook.cr
+++ src/command/run_hook.cr
@@ -2,19 +2,69 @@ module Quark
   module Command
     # run quark hook
     def self.run_hook(args)
+      hooks = {
+        "bash" => <<-BASH,
+        # run hook on pwd change
+        function _quark_hook {
+            [[ "$PREVPWD" != "$PWD" ]] && quark add
+            export PREVPWD="$PWD"
+        }
+
+        # run _quark_hook on prompt
+        PROMPT_COMMAND="_quark_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
+
+        # add q command
+        function q {
+            if [[ $# == 0 ]]; then
+                quark run
+            else
+                cd $(quark run $@)
+            fi
+        }
+        BASH
+
+        "zsh" => <<-ZSH,
+        autoload -Uz add-zsh-hook
+
+        # run quark-hook on chpwd
+        function _quark_hook {
+            quark add
+        }
+        add-zsh-hook chpwd _quark_hook
+
+        # add q command
+        function q {
+            if [[ $# == 0 ]]; then
+                quark run
+            else
+                cd $(quark run $@)
+            fi
+        }
+        ZSH
+
+        "fish" => <<-FISH,
+        # run hook on pwd change
+        function _quark_hook --on-variable PWD
+            quark add
+        end
+
+        # add q command
+        function q
+            if count $argv > /dev/null
+                cd (quark run $argv)
+            else
+                quark run
+            end
+        end
+        FISH
+      }
+
       shell = args[0]?
+      hook = hooks[shell]?
 
-      # check if hook exists
-      has_hook =
-        Dir[File.expand_path(File.join("..", "hook", "*"), __DIR__)]
-          .map { |file| File.basename(file) }
-          .includes?("hook.#{shell}")
-
-      if has_hook
-        # get hook
-        puts File.read(File.expand_path(
-          File.join("..", "hook", "hook.#{shell}"), __DIR__
-        ))
+      if hook
+        # print hook
+        puts hook
       else
         error "hook for `#{shell}` does not exist", 1
       end
