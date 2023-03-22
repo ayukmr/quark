75cd4c9a05a0b7241ac0ca6249acb726d1e54242
Ayush Kumar <github@ayushk.xyz>
2023-03-22 07:42:36 -0700

    Format clean_locations.cr

--- src/command/clean_locations.cr
+++ src/command/clean_locations.cr
@@ -2,22 +2,22 @@ module Quark
   module Command
     # clean locations
     def self.clean_locations
-        db = DB.connect("sqlite3://#{File.expand_path("~/.quark.db", home: true)}")
-        ensure_table(db)
+      db = DB.connect("sqlite3://#{File.expand_path("~/.quark.db", home: true)}")
+      ensure_table(db)
 
-        db.query("SELECT * FROM locations") do |rows|
-          rows.each do
-            path = rows.read(String)
+      db.query("SELECT * FROM locations") do |rows|
+        rows.each do
+          path = rows.read(String)
 
-            # remove location if invalid
-            if !Dir.exists?(path)
-              db.exec("DELETE FROM locations WHERE path = ?", path)
-              puts "#{"removing".red.bold} `#{path.tilde}`"
-            end
+          # remove location if invalid
+          if !Dir.exists?(path)
+            db.exec("DELETE FROM locations WHERE path = ?", path)
+            puts "#{"removing".red.bold} `#{path.tilde}`"
           end
         end
+      end
 
-        puts "#{"cleaned locations".magenta.bold}"
+      puts "#{"cleaned locations".magenta.bold}"
     end
   end
 end
